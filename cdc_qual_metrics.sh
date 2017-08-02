9dcaee8e1b85be9e23bc1b23173c954b68570792
kevinlibuit <kevin@libuit.com>
2017-08-02 09:40:26 -0400

    get reads from BaseSpace and use run_assembly_readMetrics.pl to get quality metrics

---
+++ cdc_qual_metrics.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+
+HELP="
+Generates read-quality metrics using run_assembly_readMetrics.pl from the CDC CG-Pipeline for reads in a specified BaseSpace project. 
+
+Assumes BaseSpace directory exists in home directory and is mounted to BaseSpace account.
+
+If not, run: 
+$ mkdir ~/BaseSpace
+$ basemount ~/BaseSpace
+
+...and follow prompts to properly mount BaseSpace profile to the new directory.
+
+A sym link for read files (fastq.gz) within a BaseSpace project will be in <output_dir>/raw_reads/.
+
+Final <project_id>_readMetrics.tsv will be placed within the output_dir.
+
+
+Usage:
+
+cdc_qual_metrics.sh -i <BaseSpace_project> -o <output_directory>
+
+-i: name of the BaseSpace project with reads of interest
+-o: name of the directory in which sym links will be created
+
+"
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "$HELP"
+                        exit 0
+                        ;;
+                -p)
+                        shift
+                        if test $# -gt 0; then
+                                project_id=$1
+                        else
+                                echo "no project specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --project-id*)
+                        project_id=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+ERROR="ERROR:: No readMetrics genreated. See message above or use cdc_qual_metrics -h for more info."
+
+#Link reads to output_dir
+echo "Linking reads from BaseSpace Proejct... "
+link_reads_from_bsproj.sh -i ${project_id} -o ${project_id}
+
+
+if ls ${project_id}/raw_reads/*.fastq.gz 1> /dev/null 2>&1;
+then
+  echo "Getting quality metrics..."
+  run_assembly_readMetrics.pl --fast ${project_id}/raw_reads/*.fastq.gz -e 5000000 | sort -k3,3n > ${project_id}/${project_id}_readMetrics.tsv
+  echo "Done! Quality metrics are in ${project_id}/${project_id}_readMetrics.tsv and can be viewed in Microsoft Excel." 
+else
+  echo ${ERROR}
+fi
+
+
+
---
+++ link_reads_from_bsproj.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+HELP="
+Create sym link for read files (fastq.gz) in BaseSpace project to a specified directory. Assumes BaseSpace directory exists in home directory and is mounted to BaseSpace account.
+
+If not, run: 
+$ mkdir ~/BaseSpace
+$ basemount ~/BaseSpace
+
+Follow prompts to properly mount BaseSpace profile to the new directory.
+
+Usage:
+
+link_reads_from_bsproj.sh -i <BaseSpace_project> -o <output_directory>
+
+-i: name of the BaseSpace project with reads of interest
+-o: name of the directory in which sym links will be created
+"
+
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "$HELP"
+                        exit 0
+                        ;;
+                -i)
+                        shift
+                        if test $# -gt 0; then
+                                input_dir=$1
+                        else
+                                echo "no input specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --input*)
+                        input_dir=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                -o)
+                        shift
+                        if test $# -gt 0; then
+                                output_dir=$1
+                        else
+                                echo "no output dir specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --output-dir*)
+                        output_dir=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+if ls ~/BaseSpace/Projects/${input_dir}/Samples/*/Files/*.fastq.gz 1> /dev/null 2>&1; 
+then
+  mkdir ${output_dir}/raw_reads -p
+  ln -s ~/BaseSpace/Projects/${input_dir}/Samples/*/Files/*.fastq.gz ${output_dir}/raw_reads/
+  echo "Reads from BaseSpace project ${input_dir} have been linked linked to ${output_dir}/raw_reads/raw_reads."
+
+else
+  echo "No read files found in BaseSpace project ${input_dir}. Please enter valid BaseSpace project and ensure ~/BaseSpace is properly mounted"
+fi
+
+
